8f9957a7a6c1944b44c137a6854f9b1fd6c108a5
sushrutlekurwale <83647918+sushrutlekurwale@users.noreply.github.com>
2021-10-29 11:33:15 +0530

    Update Practical 2.py

--- Practical 2.py
+++ Practical 2.py
@@ -84,3 +84,14 @@ def number():
 number()
 number()
 number()
+
+# Q: Write a program to input the basic salary of an employee and calculate the gross salary according to the given conditions:
+# Basic Salary <= 10000: HRA=20% DA=80%
+# Basic Salary is between 10001 to 20000: HRA=25% DA=90% 
+# Basic Salary >= 20001: HRA=10% DA=95%
+
+# Q: If the ages of three brothers are input through the keyboard, write a program to determine the youngest and oldest of three brothers
+
+# Q: Write a program to calculate the overtime pay of employee, overtime is paid at the rate of Rs.12.00/hour for every hour worked above 40 hours. Assume that employee do not work for fractional hours
+
+# Q: 
